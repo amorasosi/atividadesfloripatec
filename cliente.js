18319b5048d15078391a34015fdb73fd7729ec1f
Maria Carolina <kakasilvacontato@outlook.com>
2022-10-19 07:57:42 -0300

    atividade1

---
+++ cliente.js
@@ -0,0 +1,10 @@
+var nome_cliente = "Maria";
+var idade_cliente = 16;
+var prof_cliente = "Dev";
+var objetivo_cliente = "Ser Dev Senior"
+
+if(idade_cliente >= 18){
+    console.log("Cliente maior de idade")
+}else{
+    console.log("Cliente menor de idade");
+}
